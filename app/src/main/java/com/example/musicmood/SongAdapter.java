8dd30efdd5296e2d2af726ad0208f1a5e7a0e92a
CyberGlitchKripto <cserathighosh@gmail.com>
2025-07-08 12:18:38 +0530

    Initial commit - music app

---
+++ app/src/main/java/com/example/musicmood/SongAdapter.java
@@ -0,0 +1,79 @@
+package com.example.musicmood;
+
+import android.content.ContentUris;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.ContentView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.musicmood.databinding.ItemSongBinding;
+
+import java.util.List;
+
+public class SongAdapter extends RecyclerView.Adapter<SongAdapter.SongViewholder> {
+    private final List<Song> songs;
+    private final OnItemClickListerner listerner;
+
+
+    public  interface OnItemClickListerner{
+        void onItemClick(int position);
+    }
+    @NonNull
+    @Override
+    public SongAdapter.SongViewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        ItemSongBinding binding=ItemSongBinding.inflate(
+                LayoutInflater.from(parent.getContext()),parent, false);
+        return new SongViewholder(binding,listerner);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SongAdapter.SongViewholder holder, int position) {
+Song song=songs.get(position);
+holder.binding.textTitle.setText(song.title);
+holder.binding.textArtist.setText(song.artist);
+
+        Uri albumArtUri= ContentUris.withAppendedId(Uri.parse("conent://media/external/audio/albumart"),song.albumId);
+        Glide.with(holder.binding.getRoot().getContext())
+                .load(albumArtUri)
+                .circleCrop()
+                .placeholder(R.drawable.ic_music_note_24)
+                .error(R.drawable.ic_music_note_24)
+                .into(holder.binding.imageAlbumArt);
+    }
+
+    public SongAdapter(List<Song> songs, OnItemClickListerner listerner) {
+        this.songs = songs;
+        this.listerner = listerner;
+    }
+
+    @Override
+    public int getItemCount() {
+        return Songs.size();
+    }
+
+    public class SongViewholder extends RecyclerView.ViewHolder {
+        final ItemSongBinding binding;
+        final OnItemClickListerner listener;
+    public SongViewholder(ItemSongBinding binding, OnItemClickListerner listerner) {
+            super(binding.getRoot());
+            this.binding=binding;
+            this.listener=listerner;
+            binding.getRoot().setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (listerner!=null){
+                        int pos=getAdapterPosition();
+                        if (pos!=RecyclerView.NO_POSITION){
+                            listener.onItemClick(pos);
+                        }
+                    }
+                }
+            });
+        }
+    }
+}
---
+++ app/src/main/java/com/example/musicmood/SplashActivity.java
@@ -0,0 +1,36 @@
+package com.example.musicmood;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.musicmood.databinding.ActivitySplashBinding;
+
+public class SplashActivity extends AppCompatActivity {
+private  ActivitySplashBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding = ActivitySplashBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        binding.Startbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SplashActivity.this,MainActivity.class));
+            }
+        });
+    }
+}
